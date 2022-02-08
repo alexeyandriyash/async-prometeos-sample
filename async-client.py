20d0eba08ebbfe4d88bf90812245f82a3c439f70
alexeyandriyash <alexey@ndriya.sh>
2022-02-08 20:12:19 +0000

    feat: base content added

--- async-client.py
+++ async-client.py
@@ -25,7 +25,7 @@ loggingConfig = json.load(loggingConfigFile)
 
 
 logging.config.dictConfig(loggingConfig)
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 # Create a metric to track time spent and requests made.
 #FOO_SUMMARY = Summary('request_processing_seconds', 'foo summary')
@@ -38,47 +38,82 @@ bar_histogram = Histogram(name = 'bar_histogram', documentation = 'bar histogram
 #BAR_HISTOGRAM.observe(0.1)
 bazz_counter = Counter('bazz_counter', 'Description of counter')
 bazz_counter.inc()
-
 routes = web.RouteTableDef()
+
+
 
 @routes.get('/foo')
 @metrics(foo_summary)
 async def foo(request: web.Request) -> web.Response:
-    latency = random.random()
-    text="foo: {}".format(latency)
-    
-    print(text)
-    await asyncio.sleep(latency)
-
-    return web.Response(text=text)
-
+    return web.Response(text=await base_handler('foo'),
+                        content_type='text/html')
 
 @routes.get('/bar')
 @metrics(bar_histogram)
 async def bar(request: web.Request) -> web.Response:
-    latency = random.random()
-    text="bar: {}".format(latency)
-    
-    print(text)
-    await asyncio.sleep(latency)
-    
-    return web.Response(text=text)
+    return web.Response(text=await base_handler('bar'),
+                        content_type='text/html')
 
 @routes.get('/bazz')
 async def bar(request: web.Request) -> web.Response:
-    latency = random.random()
-    text="bazz: {}".format(latency)
-    print(text)
-    bazz_counter.inc()
-    await asyncio.sleep(latency)
 
-    return web.Response(text=text)
+    return web.Response(text=await base_handler('bazz'),
+                        content_type='text/html')
 
+@routes.get('/')
+async def root_handler(request: web.Request) -> web.Response:
+    return web.Response(text=base_content,
+                        content_type='text/html')
 
 
 app = web.Application()
 app.add_routes(routes)
 app.router.add_get("/metrics", aio.web.server_stats)
+
+def get_routes_list():
+    routes = list()
+    for resource in app.router.resources():
+        routes.append(resource.get_info().get("path"))
+
+    routes.remove('/metrics')
+
+    routes.sort()
+    return routes
+
+def get_base_html_content():
+    link_pattern = '<a href="{path}">{text}</a><br>'
+    routes = get_routes_list()
+
+    content = [ link_pattern.format(path = route, text = route) for route in routes]
+
+    content.append('<br>')
+    content.append('<br>')
+    content.append(link_pattern.format(path = '/metrics', text = '/metrics'))
+
+    
+    content_str = ""
+    return content_str.join(content)
+
+
+def get_extended_html_content():
+    extention = "<br><br>last route: <b>{route_name}</b>, last time: <b>{latency}</b>"
+    extended_content = get_base_html_content() + extention
+    return extended_content
+
+
+base_content = get_base_html_content()
+extended_content = get_extended_html_content()
+
+
+async def base_handler(route_name):
+    
+    latency = random.random()
+    await asyncio.sleep(latency)
+    text="bar: {}".format(latency)
+    
+    return extended_content.format(route_name = route_name, latency = latency)
+
+
 
 if __name__ == '__main__':
     print("app started")
